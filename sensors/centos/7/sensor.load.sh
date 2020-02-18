d7287c6adce64eb42b6569d03cd632e919c0dc43
ikseth <ignaciogarciahoyos@gmail.com>
2020-02-18 11:47:55 +0100

    Update sensor.load.sh
    fix uptime command extract load info

--- sensors/centos/7/sensor.load.sh
+++ sensors/centos/7/sensor.load.sh
@@ -6,28 +6,32 @@ _sensor_name=$1
 _sensor_status="CHECKING"
 
 [ -z "$2" ] && _interval=5 || _interval=$2
-[ "$3" == "ALERT" ] && _status="FAIL" || _status="MARK" ## ALLOW CHANGE IF SENSOR TRIGGER AN ALERT OR NOT
+[ "$3" == "ALERT" ] && _status="FAIL" || _status="MARK" ## ALLOW TO CHANGE IF SENSOR TRIGGER AN ALERT OR NOT
 
 _sensor_cmd=$( which uptime 2>/dev/null )
 
+### SCRIPT [ CONTROL OUTPUT TO NULL OR TO FILE ONLY LAST COMMAND SEND OUTPUT TO >1 ]
+
+# available sensor status [DISABLE|OK|UP|DOWN|ALERT|WARN] after that you can print value, if don't use status cyclops ignore it but you add value to have sensor statistics data
 if [ -z "$_sensor_cmd" ]
 then
         _sensor_status="DISABLE no cmd"
 else
-        _cores=$( grep processor /proc/cpuinfo | tail -n 1 | cut -d':' -f2 )
-        _sensor_status=$( $_sensor_cmd | cut -d':' -f5- | awk -F\, -v _i="$_interval" -v _c="$_cores" -v _s="$_status" '
+        _cores=$( grep processor /proc/cpuinfo | wc -l )
+        _sensor_status=$( $_sensor_cmd | awk -v _i="$_interval" -v _c="$_cores" -v _s="$_status" '
                 BEGIN {
                         f=0 ; _c=int(_c)
                         if ( _i == 1 ) {  f=1 }
                         if ( _i == 5 ) {  f=2 }
                         if ( _i == 15 ) { f=3 }
                 } {
-                        _load=$f
+                        for (m=1;m<=NF;m++) { if ( $m ~ "average" ) { _field=m ; break } }
+                        _load=$(_field+f)
                 } END {
                         if ( f == 0 ) {
-                                print "DISABLE no data"
+                                print "DISABLE snr stng"
                         } else {
-                                _load=int((_load*100)/_c)
+                                _load=(_load*100)/_c
                                 if ( _load > 100 ) { _status=_s } else { _status="UP" }
                                 print _status" "_load"%"
                         }
