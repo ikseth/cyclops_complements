def182abf247ead0251a08a69a47e152fbd56d26
ikseth <ignaciogarciahoyos@gmail.com>
2020-03-24 12:35:34 +0100

    Create sensor.srv.sh
    Linux service control sensor status

---
+++ sensors/opensuse/tumbleweed/sensor.srv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#@HELP@ This Sensor needs daemon name, is important the daemon behavior was an standard (status output)
+#@HELP@ This Sensor need to control the OS to execute the right command
+#@DAEMON@_sensor_name="DAEMON"
+
+_sensor_name=$1
+_sensor_status="CHECKING"
+
+source /etc/cyclops/global.cfg ## OWN EXEC ##
+
+_daemon_value=$( service $_sensor_name status 2>/dev/null >/dev/null; echo $?)
+case $_daemon_value in
+0)
+        _sensor_status="UP"
+;;
+1)
+        _sensor_status="DOWN stop"
+;;
+"")
+        _sensor_status="UNKNOWN no data"
+;;
+*)
+        _sensor_status="DOWN err.($_daemon_value)"
+;;
+esac
+
+echo $_sensor_name":"$_sensor_status"@"
